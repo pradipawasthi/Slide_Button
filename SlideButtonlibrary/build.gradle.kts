fe0ebbc87988b83845e98ce0dd6e61ecb8437ddf
pradip.awasthi <pradip@bebetta.in>
2024-03-12 14:58:56 +0530

    [Fix] : dependency update

--- SlideButtonlibrary/build.gradle.kts
+++ SlideButtonlibrary/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
-//    alias(libs.plugins.androidLibrary)
-//    alias(libs.plugins.jetbrainsKotlinAndroid)
-    id("com.android.library")
-    kotlin("android")
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
